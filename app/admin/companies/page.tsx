77b63dc987b409c7f32ff7a1e58cadee89c43c76
fatih ulker <fatihulker000@gmail.com>
2026-04-08 16:48:12 +0300

    fix companies page

---
+++ app/admin/companies/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function AdminCompaniesPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Firmalar</h1>
+      <p>Firma yönetim ekranı hazır.</p>
+    </div>
+  );
+}
